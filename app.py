44e226fd1948e951ced5606220c6fc8800473461
Super <1125699801@qq.com>
2020-06-16 14:27:08 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask  # 从flask包导入Flask类
+from flask import Flask, render_template  # 从flask包导入Flask类
 from flask import url_for
 
 app = Flask(__name__)  # 实例化类，创建一个程序对象app
 
+name = 'Super'
+movies = [{'title': 'My Neighbor Totoro', 'year': '1988'}, {'title': 'Dead Poets Society', 'year': '1989'},
+          {'title': 'A Perfect World', 'year': '1993'}, {'title': 'Leon', 'year': '1994'},
+          {'title': 'Mahjong', 'year': '1996'}, {'title': 'Swallowtail Butterfly', 'year': '1996'},
+          {'title': 'King of Comedy', 'year': '1999'}, {'title': 'Devils on the Doorstep', 'year': '1999'},
+          {'title': 'WALL-E', 'year': '2008'}, {'title': 'The Pork of Music', 'year': '2012'}, ]
+
 
 @app.route('/')
 def index():
-    return 'Hello Flask'
+    return render_template('index.html', name=name, movies=movies)
 
 
 @app.route('/user/<name>')  # 注册路由，使用装饰器绑定URL
