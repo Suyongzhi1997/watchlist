1d1240599e6c43c7a265654ba56ad75f50913c4c
Super <1125699801@qq.com>
2020-06-16 15:21:12 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,19 +1,66 @@
+import sys, os
+import click
 from flask import Flask, render_template  # 从flask包导入Flask类
 from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:
+    # 否则使用四个斜线
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)  # 实例化类，创建一个程序对象app
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型 修改的监控
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例app
 
-name = 'Super'
-movies = [{'title': 'My Neighbor Totoro', 'year': '1988'}, {'title': 'Dead Poets Society', 'year': '1989'},
-          {'title': 'A Perfect World', 'year': '1993'}, {'title': 'Leon', 'year': '1994'},
-          {'title': 'Mahjong', 'year': '1996'}, {'title': 'Swallowtail Butterfly', 'year': '1996'},
-          {'title': 'King of Comedy', 'year': '1999'}, {'title': 'Devils on the Doorstep', 'year': '1999'},
-          {'title': 'WALL-E', 'year': '2008'}, {'title': 'The Pork of Music', 'year': '2012'}, ]
+
+@app.cli.command()
+def forge():
+    db.create_all()
+    name = 'Super'
+    movies = [{'title': 'My Neighbor Totoro', 'year': '1988'}, {'title': 'Dead Poets Society', 'year': '1989'},
+              {'title': 'A Perfect World', 'year': '1993'}, {'title': 'Leon', 'year': '1994'},
+              {'title': 'Mahjong', 'year': '1996'}, {'title': 'Swallowtail Butterfly', 'year': '1996'},
+              {'title': 'King of Comedy', 'year': '1999'}, {'title': 'Devils on the Doorstep', 'year': '1999'},
+              {'title': 'WALL-E', 'year': '2008'}, {'title': 'The Pork of Music', 'year': '2012'}, ]
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done.')
+
+
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 年份
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
 
 
 @app.route('/user/<name>')  # 注册路由，使用装饰器绑定URL
